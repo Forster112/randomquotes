1af2cb93eaed10338b63f1e1a906ca6bc0d7957e
Forster112 <forstermichael112@gmail.com>
2023-06-29 17:46:40 +0100

    adjust height and title

--- src/components/atom/QuoteWrap/Index.jsx
+++ src/components/atom/QuoteWrap/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const QuoteWrap = (props) => {
   return (
-    <div className='bg-slate-700 rounded-md shadow-2xl shadow-slate-800 w-1/2 h-52 flex flex-col justify-center items-center p-6'>
+    <div className='bg-slate-700 rounded-md shadow-2xl shadow-slate-800 w-1/2 h-fit flex flex-col justify-center items-center p-6'>
       {props.children}
     </div>
   )
